32adb144ff95f4888cd4173989f5079a325947ac
LethalGhost <lethalghost@yandex.ru>
2016-02-17 10:19:04 +0500

    Fix #45
    Именем Зла, во славу Зла!

--- tools/mapmerge/1prepare_map.bat
+++
@@ -1,7 +0,0 @@
-cd ../../maps
-
-FOR %%f IN (*.dmm) DO (
-  copy %%f %%f.backup
-)
-
-pause
--- tools/mapmerge/2clean_map.bat
+++
@@ -1,8 +0,0 @@
-SET z_levels=6
-cd 
-
-FOR %%f IN (../../maps/*.dmm) DO (
-  java -jar MapPatcher.jar -clean ../../maps/%%f.backup ../../maps/%%f ../../maps/%%f
-)
-
-pause
