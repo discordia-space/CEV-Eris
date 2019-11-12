fdb23305ec139eaaa1015ad09699387bcb63b2de
Kurgis <35203040+Kurgis@users.noreply.github.com>
2019-11-12 11:46:19 -0500

    Map conversion to tgm. (#4139)
    * Map conversion.
    
    Converts dmm files to tgm.
    
    * Removes Java mapmerger

--- tools/mapmerger/clean_map_git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for MAPFILE in ../../maps/*.dmm
-do
-	MAPNAME=$(basename $MAPFILE)
-	git show HEAD:maps/$MAPNAME > tmp.dmm
-	java -jar MapPatcher.jar -clean tmp.dmm $MAPFILE $MAPFILE
-	rm tmp.dmm
-done
