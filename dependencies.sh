7543ea2ca757eb777ca3306e0dfb28c8bb928e10
LetterN <24603524+LetterN@users.noreply.github.com>
2022-08-17 09:25:22 +0800

    update rustg

--- dependencies.sh
+++ dependencies.sh
@@ -8,11 +8,11 @@ export BYOND_MAJOR=514
 export BYOND_MINOR=1568
 
 #rust_g git tag
-export RUST_G_VERSION=0.4.10
+export RUST_G_VERSION=1.0.1
 
 #node version
-export NODE_VERSION=12
-export NODE_VERSION_PRECISE=12.20.0
+export NODE_VERSION=14
+export NODE_VERSION_PRECISE=14.16.1
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.7.1
