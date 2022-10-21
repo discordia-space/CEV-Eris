492001ceb3808b145af9c682816c6646aeebd1cc
TheShown911 <61743710+TheShown911@users.noreply.github.com>
2022-10-21 23:40:25 +0200

    Revert "adds tgui (now real!) (#7685)" (#7728)
    This reverts commit 13c3e58f59ebdeb3491045939aa66e3f91d04751.

--- code/_global_vars/misc.dm
+++ code/_global_vars/misc.dm
@@ -30,6 +30,3 @@ GLOBAL_LIST_INIT(custom_kits, list(
 // LOGGING  MOVE ME //
 GLOBAL_VAR(log_directory)
 GLOBAL_PROTECT(log_directory)
-
-// TGUI MOVE ME //
-GLOBAL_DATUM(changelog_tgui, /datum/changelog)
--- tools/deploy.sh
+++ tools/deploy.sh
@@ -12,9 +12,7 @@ fi
 mkdir -p \
     $1/maps \
     $1/strings \
-	$1/config \
-    $1/tgui/public \
-    $1/tgui/packages/tgfont/dist
+	$1/config
 
 if [ -d ".git" ]; then
   mkdir -p $1/.git/logs
@@ -25,9 +23,6 @@ cp cev_eris.dmb cev_eris.rsc $1/
 cp -r maps/* $1/maps/
 cp -r strings/* $1/strings/
 cp -r config/names $1/config/
-cp -r tgui/public/* $1/tgui/public/
-cp -r tgui/packages/tgfont/dist/* $1/tgui/packages/tgfont/dist/
-
 #remove .dm files from _maps
 
 #this regrettably doesn't work with windows find
