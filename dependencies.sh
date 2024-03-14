ad593a891edf2c9dad31b5f5f1fe6cca9014967b
MLGTASTICa <ak9bc10d@yahoo.com>
2024-03-14 07:13:15 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -4,8 +4,8 @@
 #Final authority on what's required to fully build the project
 
 # byond version
-export BYOND_MAJOR=514
-export BYOND_MINOR=1568
+export BYOND_MAJOR=515
+export BYOND_MINOR=1633
 
 #rust_g git tag
 export RUST_G_VERSION=1.0.1
