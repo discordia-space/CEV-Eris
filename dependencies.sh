bd7c4167607dc0ae198748eae41c4e79bd3a4d8f
SirRichardFrancis <ahrfburton@gmail.com>
2025-06-14 13:22:28 +0300

    test

--- dependencies.sh
+++ dependencies.sh
@@ -4,22 +4,20 @@
 #Final authority on what's required to fully build the project
 
 # byond version
-export BYOND_MAJOR=514
-export BYOND_MINOR=1568
+export BYOND_MAJOR=516 #514
+export BYOND_MINOR=1659 #1568
 
 #rust_g git tag
-export RUST_G_VERSION=1.0.1
+export RUST_G_VERSION=3.9.0
 
 #node version
-export NODE_VERSION=14
-export NODE_VERSION_LTS=20.12.0
-export NODE_VERSION_COMPAT=14.16.1
+export NODE_VERSION_LTS=22.11.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.7.1
+export SPACEMAN_DMM_VERSION=suite-1.10
 
 # Extools git tag
 export EXTOOLS_VERSION=v0.0.7
 
 # Python version for mapmerge and other tools
-export PYTHON_VERSION=3.7.9
+export PYTHON_VERSION=3.9.0
